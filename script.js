c3e834bdb45c4445e4772ab1a6bc0df5b243892c
Anil Kumar <114513976+ak8058@users.noreply.github.com>
2025-02-26 21:10:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function highlight() {
     //Write your code here
-
-
+	document.querySelectorAll('strong').forEach(el=>{
+		el.style.color='rgb(0, 128, 0)';
+	});
 }
 
 
 function return_normal() {
     //Write your code here
-
+	document.querySelectorAll('strong').forEach(el=>{
+		el.style.color='rgb(0, 0, 0';
+	});
     
 }
